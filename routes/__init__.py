5d4a231ee653116c990c166eaa521bd31701d884
GSvegetable <3301296046@qq.com>
2026-08-21 05:41:16 +0800

    Update comments in __init__.py for clarity

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 
-# 创建主蓝图
+# 定义主蓝图
 main_bp = Blueprint('main', __name__)
 
-# 导入各个模块，让它们将路由注册到 main_bp 上
+# 导入各个子模块，让它们把路由注册到 main_bp 上
 from . import auth
 from . import core
 from . import admin
