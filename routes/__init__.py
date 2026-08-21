247851e46b2d40bfed00fc7a3f10b7ed27ae3559
GSvegetable <3301296046@qq.com>
2026-08-21 18:30:36 +0800

    Reorder import statements in __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,12 +1,7 @@
 from flask import Blueprint
 
-# 定义主蓝图
 main_bp = Blueprint('main', __name__)
 
-# 导入各个子模块，让它们把路由注册到 main_bp 上
-from . import auth
 from . import core
+from . import auth
 from . import admin
-from . import api
-from . import bot
-from . import ai
